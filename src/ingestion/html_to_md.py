2b7a09818402d3d61ec1f253cffe80a93b6350e3
zzzmikej <michael.silva@sptech.school>
2025-06-17 10:20:50 -0300

    Add html_to_md.py for converting HTML files to Markdown format

---
+++ src/ingestion/html_to_md.py
@@ -0,0 +1,26 @@
+import os
+from bs4 import BeautifulSoup
+import html2text
+from src.config import settings
+
+def convert_html_to_md(html_content):
+    soup = BeautifulSoup(html_content, 'html.parser')
+    markdown = html2text.html2text(str(soup))
+    return markdown
+
+def proccess_html_files(html_folder, md_folder):
+    os.makedirs(md_folder, exist_ok=True)
+
+    for root, _, files in os.walk(html_folder):
+        for file in files:
+            if file.endswith('.html'):
+                html_path = os.path.join(root, file)
+                with open(html_path, 'r', encoding='utf-8') as f:
+                    html_content = f.read()
+                
+                markdown_content = convert_html_to_md(html_content)
+                md_file_name = file.replace('.html', '.md')
+                md_path = os.path.join(md_folder, md_file_name)
+
+                with open(md_path, 'w', encoding='utf-8') as f:
+                    f.write(markdown_content)
