014739b0643381dc528c3187bd68f867b7f83970
zzzmikej <michael.silva@sptech.school>
2025-06-22 12:03:22 -0300

    Refactor main.py to enable document similarity search and update embedding function calls

--- src/main.py
+++ src/main.py
@@ -1,10 +1,17 @@
 from ingestion.download_html import download_confluence_pages
 from embeddings.generate_embeddings import update_embedding
+from rag.retriever import search_similar_documents
+from chromadb.utils.embedding_functions import SentenceTransformerEmbeddingFunction
 from config import settings
 
 if __name__ == "__main__":
-    download_confluence_pages()
-    print("✅ Processo de download e conversão concluído com sucesso!")
+    # download_confluence_pages()
+    # print("✅ Processo de download e conversão concluído com sucesso!")
 
-    update_embedding(settings.EMBEDDINGS_DIR)
-    print("✅ Embeddings gerados e armazenados com sucesso no ChromaDB.")
+    # update_embedding(settings.EMBEDDINGS_DIR)
+    # print("✅ Embeddings gerados e armazenados com sucesso no ChromaDB.")
+
+    embeder = SentenceTransformerEmbeddingFunction(settings.EMBEDDING_MODEL)
+    query = "Como fazer login?"
+    results = search_similar_documents(query, embeder, top_k=8)
+    print(results)
