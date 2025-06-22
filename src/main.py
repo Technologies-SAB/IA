f3eca9a58bb07716e41957ed4e745e023f1fe5b8
zzzmikej <michael.silva@sptech.school>
2025-06-22 18:13:56 -0300

    Remove unused imports and update main execution flow to download models directly

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 from ingestion.download_html import download_confluence_pages
 from embeddings.generate_embeddings import update_embedding
-from rag.retriever import search_similar_documents
-from chromadb.utils.embedding_functions import SentenceTransformerEmbeddingFunction
+from training.download_models import download_embeddings_model, download_llm_model
 from config import settings
 
 if __name__ == "__main__":
@@ -11,7 +10,7 @@ if __name__ == "__main__":
     # update_embedding(settings.EMBEDDINGS_DIR)
     # print("✅ Embeddings gerados e armazenados com sucesso no ChromaDB.")
 
-    embeder = SentenceTransformerEmbeddingFunction(settings.MODEL_NAME)
-    query = "Como fazer login?"
-    results = search_similar_documents(query, embeder, top_k=8)
-    print(results)
+    download_embeddings_model()
+    print("✅ Modelo de embeddings baixado e salvo com sucesso.")
+    download_llm_model()
+    print("✅ Modelo LLM baixado e salvo com sucesso.")
