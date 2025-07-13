9f826e9a741d2e395c11e4e3d01fa920552ef765
zzzmikej <michaelhenrique0022@gmail.com>
2025-07-13 12:40:45 -0300

    Feat: Fazendo import especifico para cada passo, para não sobrecarregar o sistema.

--- src/main.py
+++ src/main.py
@@ -1,30 +1,32 @@
-from rag.retriever import FaissRetriever
-from ingestion.download_html import download_confluence_pages
-from embedding.generate_embeddings import update_embedding
-from training.download_models import download_embeddings_model, download_llm_model
-from chatbot.response import Chatbot
 from config import settings
 import sys
 
 def run_pipeline(step):
     if step == 'download':
-        print("--- PASSO 1: BAIXANDO DOCUMENTAÇÃO DO CONFLUENCE ---")
-        download_confluence_pages()
-        print("✅ Processo de download e conversão concluído com sucesso!")
+        print("--- PASSO 1: BAIXANDO CONTEÚDO DO CONFLUENCE ---")
+        from ingestion.download_html import download_confluence_content
+        download_confluence_content()
+
+    elif step == 'convert':
+        print("\n--- PASSO 2: PROCESSANDO CONTEÚDO PARA MARKDOWN ---")
+        from ingestion.html_to_md import process_and_convert_to_md
+        process_and_convert_to_md()
 
     elif step == 'embed':
-        print("--- PASSO 2: GERANDO EMBEDDINGS ---")
-        # Passamos o diretório correto dos arquivos .md
+        from embedding.generate_embeddings import update_embedding
+        print("--- PASSO 3: GERANDO EMBEDDINGS ---")
         update_embedding(settings.MD_FOLDER)
         print("✅ Embeddings gerados e salvos localmente.")
 
     elif step == 'models':
-        print("--- PASSO 3: BAIXANDO MODELOS ---")
+        from training.download_models import download_embeddings_model, download_llm_model
+        print("--- PASSO 4: BAIXANDO MODELOS ---")
         download_embeddings_model()
         download_llm_model()
         print("✅ Modelos baixados e salvos com sucesso.")
 
     elif step == 'chat':
+        from chatbot.response import Chatbot
         print("--- INICIANDO CLIENTE DO CHATBOT ---")
         print("Certifique-se de que o servidor do LLM está rodando em outro terminal.")
         try:
@@ -35,7 +37,7 @@ def run_pipeline(step):
                 if query.lower() == 'sair':
                     break
                 response = bot.answer(query)
-                print(f"Assistente: {response}")
+                print(f"Host: {response}")
         except Exception as e:
             print(f"Ocorreu um erro inesperado: {e}")
     else:
